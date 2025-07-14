d80eb5d58182af21f3a7326a559b3be4d129f6c1
anugerahsurya <atmajasuryaanugerah@gmail.com>
2025-07-14 13:00:15 +0700

    Perubahan Endpoint API

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ const API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:8000"
-    : "https://738ed95210fb.ngrok-free.app"; // ← Ganti sesuai ngrok
+    : "https://1d239beff412.ngrok-free.app"; // ← Ganti sesuai ngrok
 
 (function () {
   "use strict";
